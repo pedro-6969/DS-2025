456b7ffa4572df8163370e56d61385bb18c33567
pedro-6969 <pedromiguel19712@gmail.com>
2025-08-14 15:27:11 -0300

    a

--- class/atividade-class/js/main.js
+++ class/atividade-class/js/main.js
@@ -1,1 +1,11 @@
 const c = new Carro();
+
+c.marca = prompt("Digite a marca do carro:");
+c.modelo = prompt("Digite o modelo do carro:");
+c.ano = Number(prompt("Digite o ano do carro:"));
+c.portas = Number(prompt("Digite a quantidade de portas do carro:"));
+
+console.log(c.informacoes());
+console.log(`Portas: ${c.portas}`);
+console.log(c.ligar());
+console.log(c.abrirPorta());
